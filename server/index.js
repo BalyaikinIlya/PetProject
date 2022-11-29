93e9c186a298737843893f095c116e0d63208ea0
Alexey Uzlov <auzlov@codemastersintl.com>
2022-11-29 10:26:00 +0400

    * fixed system messages

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { uuidv4 } from './utils.js';
 const m = (name, text, id) => ({ name, text, id });
 const httpServer = createServer();
 const io = new Server(httpServer, {
@@ -13,7 +14,17 @@ io.on("connection", (socket) => {
 
   socket.on("userJoined", (data, cb) => {
     cb({ userId: socket.id });
-    socket.emit("createSystemMessage", `Добро пожаловать ${data.name}`);
+    socket.emit("createSystemMessage", {
+      id: uuidv4(),
+      text: `Добро пожаловать ${data.name}`,
+      time: new Date(),
+      userId: socket.id,
+      system: true,
+    });
+  });
+
+  socket.on("newMessage", (data, cb) => {
+    console.info(data);
   });
 
   socket.on("disconnect", () => {
---
+++ server/utils.js
@@ -0,0 +1,3 @@
+export function uuidv4() {
+    return Math.random(); // todo replace with better alternative like uuid library
+}
